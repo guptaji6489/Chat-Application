328a43eb5b6774084399b7d7aecbac7c2e02dd16
guptaji6489 <guptaji6489@gmail.com>
2020-08-06 22:23:39 +0530

    firebase setup done which is running localhost:3000

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,7 +2,14 @@ import React from 'react';
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-    // add here from firebase
+    apiKey: "AIzaSyC-lDR9gzo2JqasEoimwdCCvqGKW23m6hE",
+    authDomain: "webapp-9f582.firebaseapp.com",
+    databaseURL: "https://webapp-9f582.firebaseio.com",
+    projectId: "webapp-9f582",
+    storageBucket: "webapp-9f582.appspot.com",
+    messagingSenderId: "271380362896",
+    appId: "1:271380362896:web:c54ecb9a6f701f2b4b8a15",
+    measurementId: "G-LMDT9HQ98Y"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
